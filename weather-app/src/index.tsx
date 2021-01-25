eff447517ceb46adf6dbbc413c5d8547f9c33286
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-01-25 20:06:17 +0200

    adding-folders-files

--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from './App';
+import App from './src/App';
 import {Provider} from "react-redux";
 import store from './store';
 import '../node_modules/bulma/css/bulma.min.css';
